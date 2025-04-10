d51a924dd2192e237ee18c1c81da3d6991f2d450
DanBeard <5148750+DanBeard@users.noreply.github.com>
2025-04-09 21:37:29 -0500

    Added credits page and hardcoded interfaces config

--- utils/rns_config_gen.py
+++ utils/rns_config_gen.py
@@ -3,7 +3,7 @@ import sys
 import importlib.util
 from jinja2 import Environment, Template
 from typing import Optional
-
+from io import BytesIO
 from configobj import ConfigObj
 
 dir_path = os.path.dirname(os.path.realpath(__file__)) + "/.."
@@ -29,11 +29,25 @@ def generate_rns_config(plugins: dict, retcon_profile: Optional[str] = None):
     # generate the values
     enable_transport = "yes" if mode == "transport" else "no"
     plugin_interfaces = ""
+    hardcoded_interfaces = config.get('interfaces', None)
+    
+    if hardcoded_interfaces is not None:
+        # configObj doesn't give us access to the string that built a section
+        # so we have to be hacky and re-construc it from the parsed section dict
+        section_config = ConfigObj()
+        section_config.merge(hardcoded_interfaces)
+        with BytesIO() as fout:
+            section_config.write(fout)
+            hardcoded_interfaces = fout.getvalue().decode()
+        
     
     for plugin in plugins.values():
         template_vars = plugin.get_config()
         # right now just plugin config, but maybe more later
         plugin_interfaces += template_vars.get("plugin_interfaces","")
         
-    return template.render(enable_transport=enable_transport, plugin_interfaces=plugin_interfaces)
+    return template.render(enable_transport=enable_transport, 
+                           plugin_interfaces=plugin_interfaces, 
+                           hardcoded_interfaces="" if hardcoded_interfaces is not None else hardcoded_interfaces
+                           )
 
